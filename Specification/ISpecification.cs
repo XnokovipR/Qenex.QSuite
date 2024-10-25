bf92e81bcab9daa8a1ec62738d740677f5a198e7
Radek Pivokonský <radek@pivokonsky.cz>
2024-10-25 21:13:30 +0200

    feat: add Specification
    From interface ISpecification - Drivers, protocols, modules are derived. Variables contains references

---
+++ Specification/ISpecification.cs
@@ -0,0 +1,11 @@
+﻿namespace Qenex.QSuite.Specification;
+
+public interface ISpecification
+{
+    string Guid { get; }
+    string Name { get; }
+    string Description { get; }
+    string Author { get; }
+    DateTime ReleaseDate { get; }
+    Version Version { get; }
+}
