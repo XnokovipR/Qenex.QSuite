54065ff895f9214fab80542aef5e0eca2475b356
Radek Pivokonský <radek@pivokonsky.cz>
2024-10-25 23:08:49 +0200

    feat: add module base class

---
+++ Modules/Module/IModuleBase.cs
@@ -0,0 +1,13 @@
+using Qenex.QSuite.Driver;
+using Qenex.QSuite.Specification;
+using Qenex.QSuite.Variable;
+
+namespace Qenex.QSuite.Module;
+
+public interface IModuleBase
+{
+    ISpecification Specification { get; }
+    IList<IVariableBase> Variables { get; }
+    IDriverBase Driver { get; set; }
+    
+}
---
+++ Modules/Module/ModuleBase.cs
@@ -0,0 +1,19 @@
+﻿using Qenex.QSuite.Driver;
+using Qenex.QSuite.Specification;
+using Qenex.QSuite.Variable;
+
+namespace Qenex.QSuite.Module;
+
+public class ModuleBase : IModuleBase
+{
+    public ModuleBase(ISpecification specification)
+    {
+        Specification = specification;
+        Variables = new List<IVariableBase>();
+        
+    }
+    
+    public ISpecification Specification { get; }
+    public IList<IVariableBase> Variables { get; }
+    public IDriverBase Driver { get; set; }
+}
