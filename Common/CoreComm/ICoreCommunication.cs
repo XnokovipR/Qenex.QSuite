d693e940c96159dd11fe96bf6a7b36d22d0a4242
Radek Pivokonský <radek@pivokonsky.cz>
2024-10-28 20:50:45 +0100

    feat: core communication interface
    Interface common to all drivers, protocols, and modules

---
+++ Common/CoreComm/ICoreCommunication.cs
@@ -0,0 +1,10 @@
+﻿namespace Qenex.QSuite.CoreComm;
+
+public interface ICoreCommunication : IDisposable
+{
+    bool IsEnabled { get; set; }
+    bool IsStarted { get; set; }
+    
+    Task StartAsync();
+    Task StopAsync();
+}
