31de07c19a6f1f588782790e394f599e5420fb53
Radek Pivokonský <radek@pivokonsky.cz>
2024-10-29 22:30:22 +0100

    ref: move Specification

---
+++ Specifications/Specification/ISpecification.cs
@@ -0,0 +1,32 @@
+﻿namespace Qenex.QSuite.Specification;
+
+/// <summary>
+/// The specific interface uniquely identifies Qenex modules, drivers, protocols, and also assigning a protocol to a variable.   
+/// </summary>
+public interface ISpecification
+{
+    /// <summary>
+    /// A unique identifier of the specification.
+    /// </summary>
+    string Guid { get; }
+    
+    /// <summary>
+    /// A name of the specification.
+    /// </summary>
+    string Name { get; }
+    
+    /// <summary>
+    /// A description of the specification.
+    /// </summary>
+    string Description { get; }
+    
+    /// <summary>
+    /// The release date of the specification.
+    /// </summary>
+    DateTime ReleaseDate { get; }
+    
+    /// <summary>
+    /// The version of the specification.
+    /// </summary>
+    Version Version { get; }
+}
--- Specifications/Specification/SpecificationBase.cs
+++ Specifications/Specification/SpecificationBase.cs
@@ -5,7 +5,6 @@ public class SpecificationBase : ISpecification
     public string Guid { get; }
     public string Name { get; }
     public string Description { get; }
-    public string Author { get; }
     public DateTime ReleaseDate { get; }
     public Version Version { get; }
 }
