178a2b4479b0ad9ef812be0450eb41dc13dc2b10
Radek Pivokonský <radek@pivokonsky.cz>
2024-10-29 22:31:38 +0100

    feat: new Protocol-Variable-Specification

---
+++ Specifications/ProtocolVariableSpecification/ICommSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Qenex.QSuite.Specification;
+
+namespace Qenex.QSuite.ProtocolVariableSpecification;
+
+/// <summary>
+/// A Communication specification uniquely identifies a variable in a given protocol.
+/// It contains the protocol specification, and information how to communicate with the variable through the protocol.
+/// </summary>
+public interface ICommSpecification
+{
+    /// <summary>
+    /// Link to a protocol specification.
+    /// </summary>
+    ISpecification ProtocolSpecification { get; set; }
+}
