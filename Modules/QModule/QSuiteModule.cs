df97298f1fcad78a654529538becfb283228c054
Radek Pivokonský <radek@pivokonsky.cz>
2024-10-30 21:54:54 +0100

    feat: add QModule
    This module is directly used in QSuite

---
+++ Modules/QModule/QSuiteModule.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using Qenex.QSuite.Driver;
+using Qenex.QSuite.Module;
+using Qenex.QSuite.Variable;
+
+namespace Qenex.QSuite.QModule;
+
+public class QSuiteModule : ModuleBase
+{
+    public override void AddVariable(IVariableBase variable)
+    {
+        Variables.Add(variable);
+    }
+    
+    public override void AddVariableRange(IEnumerable<IVariableBase> variables)
+    {
+        foreach (var variable in variables)
+        {
+            Variables.Add(variable);
+        }
+    }
+
+    public override void RemoveVariable(IVariableBase variable)
+    {
+        Variables.Remove(variable);
+    }
+
+    public override void AddDriver(IDriverBase driver)
+    {
+        Driver ??= driver;
+    }
+
+    public override void RemoveDriver()
+    {
+        Driver = null;
+    }
+
+    public override Task StartAsync(CancellationToken ct = default)
+    {
+        var task = Driver?.StartAsync(ct);
+        return task ?? Task.CompletedTask;
+    }
+
+    public override Task StopAsync(CancellationToken ct = default)
+    {
+        var task = Driver?.StopAsync(ct);
+        return task ?? Task.CompletedTask;
+    }
+
+    public override void Dispose()
+    {
+        Driver?.Dispose();
+    }
+}
