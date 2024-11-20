cbe4321a3807efe5b4c92ac6b78fd8c8697c8e46
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-20 22:33:09 +0100

    ref: new UnifiedModule
    generalised module, use for both QSuiteModule and independent module executed in service

--- Modules/QModule/QSuiteModule.cs
+++ Modules/QModule/QSuiteModule.cs
@@ -2,68 +2,14 @@
 using Qenex.QSuite.Driver;
 using Qenex.QSuite.Module;
 using Qenex.QSuite.Variable;
+using Qenex.QSuite.UnifModule;
 
 namespace Qenex.QSuite.QModule;
 
-public class QSuiteModule : ModuleBase
+/// <summary>
+/// QSuite module servers as a model fot QSuite graphic environment
+/// </summary>
+public sealed class QSuiteModule : UnifiedModule
 {
-    #region Variables
-
-    public override void AddVariable(IVariableBase variable)
-    {
-        Variables.Add(variable);
-    }
     
-    public override void AddVariableRange(IEnumerable<IVariableBase> variables)
-    {
-        foreach (var variable in variables)
-        {
-            Variables.Add(variable);
-        }
-    }
-
-    public override void RemoveVariable(IVariableBase variable)
-    {
-        Variables.Remove(variable);
-    }
-
-    #endregion
-
-    #region Driver
-
-    public override void AddDriver(IDriverBase driver)
-    {
-        Drivers.Add(driver);
-    }
-
-    public override void RemoveDriver(IDriverBase driver)
-    {
-        Drivers.Remove(driver);
-    }
-
-    #endregion
-
-    #region Module control
-
-    public override async Task StartAsync(CancellationToken ct = default)
-    {
-        var tasks = Drivers.Select(driver => driver.StartAsync(ct));
-        await Task.WhenAll(tasks);
-    }
-
-    public override async Task StopAsync(CancellationToken ct = default)
-    {
-        var tasks = Drivers.Select(driver => driver.StopAsync(ct));
-        await Task.WhenAll(tasks);
-    }
-
-    public override void Dispose()
-    {
-        foreach (var driver in Drivers)
-        {
-            driver.Dispose();
-        }
-    }
-
-    #endregion
 }
---
+++ Modules/UnifModule/UnifiedModule.cs
@@ -0,0 +1,71 @@
+﻿using Qenex.QSuite.Driver;
+using Qenex.QSuite.Module;
+using Qenex.QSuite.Variable;
+
+namespace Qenex.QSuite.UnifModule;
+
+/// <summary>
+/// Unified model is common for both QSuiteModel and also for Model working as a independent module executed in service.
+/// </summary>
+public class UnifiedModule : ModuleBase
+{
+    #region Variables
+
+    public override void AddVariable(IVariableBase variable)
+    {
+        Variables.Add(variable);
+    }
+    
+    public override void AddVariableRange(IEnumerable<IVariableBase> variables)
+    {
+        foreach (var variable in variables)
+        {
+            Variables.Add(variable);
+        }
+    }
+
+    public override void RemoveVariable(IVariableBase variable)
+    {
+        Variables.Remove(variable);
+    }
+
+    #endregion
+
+    #region Driver
+
+    public override void AddDriver(IDriverBase driver)
+    {
+        Drivers.Add(driver);
+    }
+
+    public override void RemoveDriver(IDriverBase driver)
+    {
+        Drivers.Remove(driver);
+    }
+
+    #endregion
+
+    #region Module control
+
+    public override async Task StartAsync(CancellationToken ct = default)
+    {
+        var tasks = Drivers.Select(driver => driver.StartAsync(ct));
+        await Task.WhenAll(tasks);
+    }
+
+    public override async Task StopAsync(CancellationToken ct = default)
+    {
+        var tasks = Drivers.Select(driver => driver.StopAsync(ct));
+        await Task.WhenAll(tasks);
+    }
+
+    public override void Dispose()
+    {
+        foreach (var driver in Drivers)
+        {
+            driver.Dispose();
+        }
+    }
+
+    #endregion
+}
