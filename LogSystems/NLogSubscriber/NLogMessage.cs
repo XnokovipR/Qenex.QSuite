89280ea1db12d7f0ac6f84d3c20202098429d571
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-21 19:36:17 +0100

    feat: Nlog subscriber

---
+++ LogSystems/NLogSubscriber/NLogMessage.cs
@@ -0,0 +1,14 @@
+﻿using Qenex.QSuite.LogSystem;
+
+namespace Qenex.QSuite.NLogSubscriber;
+
+public sealed class NLogMessage: LogMessage
+{
+    public NLogMessage(DateTime timestamp, LogLevel level, string message, Exception? exception = null)
+    {
+        Timestamp = timestamp;
+        Level = level;
+        Message = message;
+        Exception = exception;
+    }
+}
---
+++ LogSystems/NLogSubscriber/NLogSubscriber.cs
@@ -0,0 +1,40 @@
+﻿using Qenex.QSuite.LogSystem;
+using NLog;
+using Logger = NLog.Logger;
+using SystemLog = Qenex.QSuite.LogSystem;
+
+namespace Qenex.QSuite.NLogSubscriber;
+
+/// <summary>
+/// NLogSubscriber logs message via NLog library into files.
+/// </summary>
+public class NLogSubscriber : ILogSubscriber
+{
+	private static readonly Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+	private static readonly Dictionary<SystemLog.LogLevel, Action<Exception, string>> logLevelToActionConverter = new()
+	{
+		{SystemLog.LogLevel.Trace, logger.Trace},
+		{SystemLog.LogLevel.Debug, logger.Debug},
+		{SystemLog.LogLevel.Info, logger.Info},
+		{SystemLog.LogLevel.Warn, logger.Warn},
+		{SystemLog.LogLevel.Error, logger.Error},
+		{SystemLog.LogLevel.Fatal, logger.Fatal}
+	};
+
+	public void Log(ILogMessage message)
+	{
+		LogMessage(message);
+	}
+
+	public async Task LogAsync(ILogMessage message, CancellationToken ct)
+	{
+		await Task.Run(() => { LogMessage(message); }, ct);
+	}
+
+	private void LogMessage(ILogMessage message)
+	{
+		var msg = message is NLogMessage ? "NLogMessage: " + message.Message : message.Message;
+		logLevelToActionConverter[message.Level]?.Invoke(message.Exception, msg);
+	}
+}
