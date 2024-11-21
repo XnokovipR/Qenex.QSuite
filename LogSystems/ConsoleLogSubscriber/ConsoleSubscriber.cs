c77f851363c2d8b43eb779b0937505e3eb20a96f
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-21 19:36:00 +0100

    feat: console log subscriber

---
+++ LogSystems/ConsoleLogSubscriber/ConsoleSubscriber.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Qenex.CmLibs.DebugFeatures;
+using Qenex.QSuite.LogSystem;
+
+namespace Qenex.QSuite.ConsoleLogSubscriber;
+
+public class ConsoleSubscriber : ILogSubscriber
+{
+	private static readonly Dictionary<LogLevel, ConsoleColor> levelToColorConverter = new()
+	{
+		[LogLevel.Debug] = ConsoleColor.Green,
+		[LogLevel.Trace] = ConsoleColor.Cyan,
+		[LogLevel.Info] = ConsoleColor.Gray,
+		[LogLevel.Warn] = ConsoleColor.Yellow,
+		[LogLevel.Error] = ConsoleColor.Red,
+		[LogLevel.Fatal] = ConsoleColor.Magenta,
+	};
+
+	public void Log(ILogMessage message)
+	{
+		var msg = new StringBuilder($"{message.Timestamp:HH:mm:ss.ffff}");
+		msg.Append($"\t{message.Level}\t{message.Message}");
+		if (message.Exception != null)
+		{
+			msg.Append($" ({message.Exception.Message})");
+		}
+		ConsoleHelper.ColoredWriteLine(levelToColorConverter[message.Level], ConsoleColor.Black, 
+			msg.ToString());
+	}
+
+	public Task LogAsync(ILogMessage message, CancellationToken ct)
+	{
+		throw new NotSupportedException("ConsoleSubscriber does not support async logging.");
+	}
+}
