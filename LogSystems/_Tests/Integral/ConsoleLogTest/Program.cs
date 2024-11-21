8e91ba388548f2f0b0eea045b43eae57cce62470
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-21 19:47:59 +0100

    test: Console log test

---
+++ LogSystems/_Tests/Integral/ConsoleLogTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Qenex.QSuite.ConsoleLogSubscriber;
+using Qenex.QSuite.LogSystem;
+
+namespace ConsoleLogTest;
+
+/// <summary>
+/// Program Test simple console log.
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger = new Logger();
+        logger.RegisterSubscriber(new ConsoleSubscriber());
+
+        var logMessage = new ConsoleMessage(LogLevel.Info, "Hello, World!");
+        logger.Log(logMessage);
+            
+        Console.WriteLine("End!");
+    }
+}
