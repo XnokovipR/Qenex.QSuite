a91727f32c6a39e3aa82b7ae50625dc65453f0e7
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-22 21:59:08 +0100

    ref: simplify

---
+++ Modules/ModuleJsonHandler/JsonModuleConverters/SpecificationConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Qenex.QSuite.Specification;
+
+namespace Qenex.QSuite.ModuleJsonHandler.JsonModuleConverters;
+
+public class SpecificationConverter : JsonConverter<ISpecification>
+{
+    public override ISpecification Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        // Deserialize as SpecificationBase
+        return JsonSerializer.Deserialize<SpecificationBase>(ref reader, options) ?? throw new InvalidOperationException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, ISpecification value, JsonSerializerOptions options)
+    {
+        // Serialize as SpecificationBase
+        JsonSerializer.Serialize(writer, (SpecificationBase)value, options);
+    }
+}
--- Specifications/Specification/SpecificationBase.cs
+++ Specifications/Specification/SpecificationBase.cs
@@ -1,10 +1,27 @@
+using System.Text.Json.Serialization;
+
 namespace Qenex.QSuite.Specification;
 
 public class SpecificationBase : ISpecification
 {
-    public Guid Gid { get; init; }
-    public string Name { get; init; }
-    public string Description { get; init; }
-    public DateTime ReleaseDate { get; init; }
-    public Version Version { get; init; }
+    [JsonPropertyName("gid")]
+    public Guid Gid { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+    
+    [JsonPropertyName("description")]
+    public string Description { get; set; } = string.Empty;
+    
+    [JsonPropertyName("createdOn")]
+    public DateTime CreatedOn { get; set; }
+    
+    [JsonPropertyName("version")]
+    public Version Version { get; set; } = null!;
+    
+    [JsonPropertyName("author")]
+    public string? Author { get; set; }
+    
+    [JsonPropertyName("company")]
+    public string? Company { get; set; }
 }
