8da530fde1c231d0c6fdb750ba4f62eb2c20e8bc
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-22 20:24:35 +0100

    test: test json module reading

---
+++ Modules/_Tests/Integral/JsonSerializationTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using Qenex.QSuite.ModuleJsonHandler.JsonModuleConverters;
+using Qenex.QSuite.ModuleJsonHandler.JsonModuleStructure;
+
+namespace JsonSerializationTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var options = new JsonSerializerOptions
+        {
+            Converters = { new VariableTypeConverter() },
+            WriteIndented = true
+        };
+        
+        string filePath = "ModuleTest.json";
+        var json = File.ReadAllText(filePath);
+        var moduleSettings = JsonSerializer.Deserialize<ModuleSettings>(json, options);
+    }
+}
