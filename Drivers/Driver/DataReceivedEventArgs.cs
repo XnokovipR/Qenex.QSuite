6a4634c4ad1ba260da0256c0f4b67bdc876159ca
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-24 00:27:19 +0100

    feat: event receiving data

---
+++ Drivers/Driver/DataReceivedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace Qenex.QSuite.Driver;
+
+public class DataReceivedEventArgs<T> : EventArgs
+{
+    public T Data { get; }
+
+    public DataReceivedEventArgs(T data)
+    {
+        Data = data;
+    }
+    
+}
