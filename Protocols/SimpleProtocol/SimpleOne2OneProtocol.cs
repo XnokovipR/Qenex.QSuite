29808fa386ee412a798078648c1f8c7ed587e722
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-24 00:26:07 +0100

    feat: new simple protocol

---
+++ Protocols/SimpleProtocol/SimpleOne2OneProtocol.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Qenex.QSuite.Protocol;
+using Qenex.QSuite.Specification;
+
+namespace SimpleProtocol;
+
+public class SimpleOne2OneProtocol : ProtocolBase
+{
+    public SimpleOne2OneProtocol()
+    {
+        Specification = new SpecificationBase()
+        {
+            Gid = new Guid("dee33606-0965-48a1-a829-020d99b8dd44"),
+            Name = "Simple One to One Protocol",
+            Description = "No conversion of data is done. Data is sent as is.",
+            CreatedOn = new DateTime(2021, 11, 23),
+            Version = Assembly.GetExecutingAssembly().GetName().Version ?? new Version(1, 0, 1),
+            Author = "Qenex",
+            Company = "QENEX Ltd."
+        };
+    }
+}
