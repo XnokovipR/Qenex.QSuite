e01d5b411acbf87fbedefa39d885de7cad568ed4
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-25 00:12:57 +0100

    ref: temp. remove logger

--- Drivers/ZmqClientDriver/ZeroMqClientDriver.cs
+++ Drivers/ZmqClientDriver/ZeroMqClientDriver.cs
@@ -10,7 +10,7 @@ namespace Qenex.QSuite.ZmqClientDriver;
 /// </summary>
 public class ZeroMqClientDriver : DriverBase
 {
-    public ZeroMqClientDriver(ILogger? logger = null) : base(logger)
+    public ZeroMqClientDriver()
     {
         Specification = new SpecificationBase()
         {
