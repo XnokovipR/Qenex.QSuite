2b504fbdedb43b8ddafbac79ef01c1cfb7c10d5b
Radek Pivokonský <radek@pivokonsky.cz>
2024-11-25 00:19:18 +0100

    feat: ctor

--- Modules/Module/ModuleBase.cs
+++ Modules/Module/ModuleBase.cs
@@ -8,17 +8,22 @@ namespace Qenex.QSuite.Module;
 
 public abstract class ModuleBase : IModuleBase
 {
+    public ModuleBase()
+    {
+        Drivers = new List<IDriverBase>();
+    }
+    
     public bool ExitRequested { get; set; } = false;
     
     public bool IsEnabled { get; set; } = false;
     
     public bool IsStarted { get; set; } = false;
     
-    public ISpecification Specification { get; init; } = null!;
+    public ISpecification Specification { get; set; } = null!;
     
-    public IList<IVariableBase> Variables { get; init; } = null!;
+    public IList<IVariableBase> Variables { get; set; } = null!;
     
-    public IList<IDriverBase> Drivers { get; init;  } = null!;
+    public IList<IDriverBase> Drivers { get; set;  } = null!;
 
     public abstract void AddVariable(IVariableBase variable);
     public abstract void AddVariableRange(IEnumerable<IVariableBase> variables);
