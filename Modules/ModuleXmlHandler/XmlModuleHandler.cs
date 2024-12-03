74547cc7270f95de28055021d51d7772354fa0bf
Radek Pivokonský <radek@pivokonsky.cz>
2024-12-03 23:24:21 +0100

    feat: add create Variable from XmlVariable
    Create specific Variable type based on xml.
    Create specific Values in Variable

--- Modules/ModuleXmlHandler/XmlModuleHandler.cs
+++ Modules/ModuleXmlHandler/XmlModuleHandler.cs
@@ -3,6 +3,8 @@ using Qenex.QSuite.LogSystem;
 using Qenex.QSuite.Module;
 using Qenex.QSuite.ModuleXmlHandler.XmlStructure;
 using Qenex.QSuite.Protocol;
+using Qenex.QSuite.QVariables;
+using Qenex.QSuite.QVariables.Values;
 using Qenex.QSuite.UnifModule;
 
 namespace Qenex.QSuite.ModuleXmlHandler;
@@ -39,14 +41,60 @@ public class XmlModuleHandler
         module.Specification.CreatedOn = xmlModule.CreatedOn;
         
         // Add variables
-        foreach (var xmlVariable in xmlModule.Variables)
-        {
-
-        }
+        module.AddVariableRange(CreateVariables(xmlModule.Variables));
 
 
 
         return module;
+    }
+
+    private List<IVariableBase> CreateVariables(IEnumerable<XmlVariable> xmlVariables)
+    {
+        var variables = new List<IVariableBase>();
+        
+        foreach (var xmlVariable in xmlVariables)
+        {
+            if (!XmlModuleGlobal.TypeOfXmlVariable2VariableEnumDict.TryGetValue(xmlVariable.GetType(), out var variableType))
+            {
+                logger?.Log(LogLevel.Error, $"Variable type {xmlVariable.GetType()} is not supported");
+                continue;
+            }
+
+            try
+            {
+                var variable = VariablesGlobal.CreateInstance(variableType);
+                variable.Id = xmlVariable.Id;
+                variable.Namespace = xmlVariable.Namespace;
+                variable.Name = xmlVariable.Name;
+                variable.Label = xmlVariable.Label;
+                variable.Description = xmlVariable.Description;
+
+                if (variable is ScalarVariable scalarVariable && xmlVariable is XmlScalarVariable xmlScalarVariable)
+                {
+                    scalarVariable.Size = xmlScalarVariable.Size;
+                    scalarVariable.Values = CreateScalarValues(xmlScalarVariable.Values);
+                }
+                
+                
+                variables.Add(variable);
+            }
+            catch (ArgumentException e)
+            {
+                logger?.Log(LogLevel.Error, $"Error creating variable {xmlVariable.Name}: {e.Message}");
+            }
+            
+        }
+
+        return variables;
+    }
+
+    private IValuesBase CreateScalarValues(XmlValues xmlValues)
+    {
+        var values = ValuesGlobal.CreateInstance(Enum.Parse<ValuesGlobal.ValueDataType>(xmlValues.DataType.ToString()));
+        
+        
+
+        return values;
     }
 
 }
