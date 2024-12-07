5630772888ee5d1583b7a0bc7920287891b86c7b
Radek Pivokonský <radek@pivokonsky.cz>
2024-12-08 00:14:42 +0100

    feat: new drv & protocol for real comm

---
+++ Drivers/PeakCanDriver/CanDriver.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Qenex.QSuite.Driver;
+using Qenex.QSuite.Specification;
+
+namespace Qenex.QSuite.Drivers.PeakCanDriver;
+
+public class CanDriver : DriverBase
+{
+    public CanDriver()
+    {
+        Specification = new SpecificationBase()
+        {
+            Name = "Peak CAN Driver",
+            Description = "Peak CAN Driver",
+            CreatedOn = new DateTime(2021, 11, 21),
+            Version = Assembly.GetExecutingAssembly().GetName().Version ?? new Version(1, 0, 1),
+            Author = "Qenex",
+            Company = "QENEX Ltd."
+        };
+    }
+    public override Task StartAsync(CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task StopAsync(CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Send<T>(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task SendAsync<T>(T data, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Protocols/XcpProtocol/Xcp.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Qenex.QSuite.Protocol;
+using Qenex.QSuite.Specification;
+
+namespace Qenex.QSuite.Protocols.XcpProtocol;
+
+public class Xcp : ProtocolBase
+{
+    public Xcp()
+    {
+        Specification = new SpecificationBase()
+        {
+            Name = "XCP Protocol",
+            Description = "XCP Protocol",
+            CreatedOn = new DateTime(2021, 11, 23),
+            Version = Assembly.GetExecutingAssembly().GetName().Version ?? throw new Exception("Version not found"),
+            Author = "Qenex",
+            Company = "QENEX Ltd."
+        };
+    }
+}
