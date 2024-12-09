8ff2686f54198c0763b2cbf3ca6f91ff8fb2783d
Radek Pivokonský <radek@pivokonsky.cz>
2024-12-09 22:40:58 +0100

    fix: forget props

--- Modules/Module/IModuleBase.cs
+++ Modules/Module/IModuleBase.cs
@@ -3,7 +3,7 @@ using Qenex.QSuite.ComponentSpecification;
 using Qenex.QSuite.Driver;
 using Qenex.QSuite.Specification;
 using Qenex.QSuite.QVariables;
-using Qenex.QSuite.Presentation;
+using Qenex.QSuite.ValuePresentation;
 using Qenex.QSuite.ValueConversion;
 
 namespace Qenex.QSuite.Module;
@@ -12,10 +12,14 @@ public interface IModuleBase : IComponentSpecification, ICoreCommunication
 {
     IList<IVariableBase> Variables { get; set; }
     IList<IDriverBase> Drivers { get; set; }
+    IList<IPresentation> Presentations { get; set; }
+    IList<IValConversion> Conversions { get; set; }
+    
     
     void AddVariable(IVariableBase variable);
     void AddVariables(IEnumerable<IVariableBase> variables);
     void RemoveVariable(IVariableBase variable);
+    
     void AddDriver(IDriverBase driver);
     void AddDrivers(IEnumerable<IDriverBase> drivers);
     void RemoveDriver(IDriverBase driver);
