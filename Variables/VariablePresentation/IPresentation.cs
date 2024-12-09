b7d7a6cca6c64c9a5aca983d56643c6b439a9e80
Radek Pivokonský <radek@pivokonsky.cz>
2024-12-09 21:49:18 +0100

    feat: add variable presentation

---
+++ Variables/VariablePresentation/IPresentation.cs
@@ -0,0 +1,11 @@
+﻿namespace Qenex.QSuite.QVariables.Presentation;
+
+public interface IPresentation
+{
+    string Name { get; set; }
+    string Label { get; set; }
+    int Min { get; set; }
+    int Max { get; set; }
+    string PrintFormat { get; set; }
+    string Unit { get; set; }
+}
---
+++ Variables/VariablePresentation/Presentation.cs
@@ -0,0 +1,11 @@
+﻿namespace Qenex.QSuite.QVariables.Presentation;
+
+public class Presentation : IPresentation
+{
+    public string Name { get; set; } = string.Empty;
+    public string Label { get; set; } = string.Empty;
+    public int Min { get; set; }
+    public int Max { get; set; }
+    public string PrintFormat { get; set; } = string.Empty;
+    public string Unit { get; set; } = string.Empty;
+}
