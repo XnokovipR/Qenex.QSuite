98dd33670fb8439be4a6f0b1967762ebd65baf8b
Radek Pivokonský <radek@pivokonsky.cz>
2024-12-25 01:29:22 +0100

    test: test of a specific driver...

--- Modules/_Tests/Integral/CreateRealModuleTest/Program.cs
+++ Modules/_Tests/Integral/CreateRealModuleTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Threading.Channels;
 using Qenex.QLibs.XmlInOut;
 using Qenex.QSuite.ConsoleLogSubscriber;
 using Qenex.QSuite.Driver;
@@ -8,6 +9,7 @@ using Qenex.QSuite.ModuleXmlHandler.XmlStructure;
 using Qenex.QSuite.PluginManager;
 using Qenex.QSuite.Protocol;
 using Qenex.QSuite.UnifModule;
+using Qenex.QSuite.Common.CoreComm;
 
 namespace Qenex.QSuite.CreateRealModuleTest;
 
@@ -15,18 +17,36 @@ class Program
 {
     static void Main(string[] args)
     {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        
         Logger logger = new Logger(LogLevel.Trace);
         logger.RegisterSubscriber(new ConsoleSubscriber());
         
         //Load drivers and protocols
         var pluginManager = new PluginLoader(logger);
-        var drivers = pluginManager.LoadPlugins<IDriverBase>("./Drivers");
-        var protocols = pluginManager.LoadPlugins<IProtocolBase>("./Protocols");
+        var driversDetails = pluginManager.GetPluginDetails<IDriverBase>("./Drivers");
+        var protocolsDetails = pluginManager.GetPluginDetails<IProtocolBase>("./Protocols");
         
         var xmlModule = XmlInOut<XmlModule>.LoadFromFile(@"..\..\..\..\..\..\ModuleXmlHandler\Docs\ModuleTest.xml");
         
-        var xmlModuleHandler = new XmlModuleHandler(drivers, protocols, logger);
+        var xmlModuleHandler = new XmlModuleHandler(driversDetails, protocolsDetails, logger);
         var realModule = xmlModuleHandler.CreateModule<UnifiedModule>(xmlModule);
+
+        var zmqDriver = realModule?.Drivers.First(d => d.Specification.Name == "ZeroMQ Client Driver");
+        if (zmqDriver == null) return;
         
+        zmqDriver.StartAsync(cts.Token);
+
+        Console.WriteLine("Press ESC to exit");
+        Console.ReadKey();
+        
+        // Cancel the driver
+        //cts.Cancel();
+        
+        // Correct way to stop the driver
+        zmqDriver.StopAsync(cts.Token);
+        
+        Console.WriteLine("Cancelled");
+        Console.ReadKey();
     }
 }
