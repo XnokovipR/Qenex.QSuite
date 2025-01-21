33518d57575019169d1b59c848c3a74e268f5e59
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-01-21 22:27:05 +0100

    test: add driver test

---
+++ Drivers/_Tests/Integral/DriverTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace DriverTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("NetMQ driver test");
+        var request = new RequestSocket();
+        var poller = new NetMQPoller();
+        
+    }
+}
