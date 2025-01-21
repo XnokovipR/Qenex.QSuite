dae37688f2f72d2f2f3539300c1fa39679f11fca
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-01-21 22:27:45 +0100

    feat: create ZeroMQ server driver

---
+++ Drivers/ZmqServerDriver/ZeroMqServerDriver.cs
@@ -0,0 +1,124 @@
+﻿using System.Reflection;
+using NetMQ;
+using NetMQ.Sockets;
+using Qenex.QSuite.Driver;
+using Qenex.QSuite.LogSystem;
+using Qenex.QSuite.Specification;
+
+namespace Qenex.QSuite.Drivers.ZmqServerDriver;
+
+public class ZeroMqServerDriver : DriverBase
+{
+    #region Fields
+
+    
+    private string settings = string.Empty;
+    private string encryptedSettings = string.Empty;
+    
+    private ResponseSocket sendSocket;
+    private RequestSocket receiveSocket;
+    private NetMQPoller poller;
+
+    #endregion
+    
+    #region Constructors
+
+    public ZeroMqServerDriver()
+    {
+        Specification = new SpecificationBase()
+        {
+            Name = "ZeroMQ Server Driver",
+            Description = "ZeroMQ Server Driver",
+            CreatedOn = new DateTime(2024, 12, 25),
+            Version = Assembly.GetExecutingAssembly().GetName().Version ?? new Version(1, 0, 1),
+            Author = "Qenex",
+            Company = "QENEX Ltd."
+        };
+    }
+
+    #endregion
+
+    public override void SetConfiguration(string rawSettings, string rawEncryptedSettings)
+    {
+        settings = $"zmq-server: {rawSettings}";
+        encryptedSettings = $"encrypted-zmq-server: {rawEncryptedSettings}";
+    }
+
+    #region Driver control
+
+    public override async Task StartAsync(CancellationToken ct = default)
+    {
+        if (!IsEnabled) return;
+
+        try
+        {
+            sendSocket = new ResponseSocket();
+            
+            receiveSocket = new RequestSocket();
+            receiveSocket.ReceiveReady += (sender, args) =>
+            {
+                var message = args.Socket.ReceiveFrameString();
+                Logger?.Log(LogLevel.Info, $"Received message: {message}");
+            };
+            poller = new NetMQPoller {receiveSocket};
+
+            await RunClientAsync(ct);
+        }
+        catch (Exception e)
+        {
+            Logger?.Log(LogLevel.Error, "ZeroMqServerDriver failed to start.", e);
+        }
+    }
+
+    public override Task StopAsync(CancellationToken ct = default)
+    {
+        try
+        {
+            poller.Stop();
+        }
+        catch (Exception e)
+        {
+            Logger?.Log(LogLevel.Error, "ZeroMqServerDriver failed to stop.", e);
+        }
+        
+        return Task.CompletedTask;
+    }
+
+    public override void Dispose()
+    {
+        poller.Dispose();
+    }
+
+    #endregion
+
+    #region Communication
+
+    public override void Send<T>(T data)
+    {
+        
+    }
+
+    public override Task SendAsync<T>(T data, CancellationToken ct = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    #endregion
+
+    #region Private methods
+
+    // All communication with the ZeroMQ library should be done in this method
+    private async Task RunClientAsync(CancellationToken ct)
+    {
+        await Task.Run(() =>
+        {
+            IsStarted = true;
+
+            poller.Run();
+            
+            IsStarted = false;
+        }, ct);
+    }
+
+    #endregion
+}
