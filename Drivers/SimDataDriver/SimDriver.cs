7ab1757d7a9883c372b2114c016f00d51808e50e
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-01-25 12:25:12 +0100

    feat: add simulation driver

---
+++ Drivers/SimDataDriver/SimDriver.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+using Qenex.QSuite.Specification;
+
+namespace Qenex.QSuite.Drivers.SimDataDriver;
+
+public class SimDriver : DriverBase
+{
+    #region Fields
+
+    private string settings = string.Empty;
+    private string encryptedSettings = string.Empty;
+
+    #endregion
+    
+    #region Constructors
+
+    public SimDriver()
+    {
+        Specification = new SpecificationBase()
+        {
+            Name = "SimulDataDriver",
+            Label = "Simulation Data Driver",
+            Description = "Peak CAN Driver",
+            CreatedOn = new DateTime(2021, 11, 21),
+            Version = Assembly.GetExecutingAssembly().GetName().Version ?? new Version(1, 0, 1),
+            Author = "Qenex",
+            Company = "QENEX Ltd."
+        };
+    }
+
+    #endregion
+    public override void SetConfiguration(string rawSettings, string rawEncryptedSettings)
+    {
+        settings = $"can: {rawSettings}";
+        encryptedSettings = $"encrypted-can: {rawEncryptedSettings}";
+    }
+
+    #region Driver control
+
+    public override Task StartAsync(CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task StopAsync(CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+
+    #region Communication
+
+    public override void Send<T>(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task SendAsync<T>(T data, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion    
+}
