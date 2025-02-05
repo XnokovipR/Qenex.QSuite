e49187c9fa562c67e7838433cfa971e6a2c219de
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-02-05 20:47:53 +0100

    style: add regions

--- Drivers/Driver/IDriverBase.cs
+++ Drivers/Driver/IDriverBase.cs
@@ -8,18 +8,29 @@ namespace Qenex.QSuite.Drivers;
 
 public interface IDriverBase : ICoreCommunication, IComponentSpecification
 {
+    #region Properties
+
     int Id { get; set; }
     string Label { get; set; }
     IList<IProtocolBase> Protocols { get; init; }
-    
+
+    #endregion
+
+    #region Configuration
+
     void SetConfiguration(string rawSettings, string rawEncryptedSettings);
     void AddProtocol(IProtocolBase protocol);
     void AddProtocols(IEnumerable<IProtocolBase> protocols);
     void RemoveProtocol(IProtocolBase protocol);
     void RemoveProtocol(string protocolName);
-    
+
+    #endregion
+
+    #region Send / Receive data
     void Send<T>(T data);
     Task SendAsync<T>(T data, CancellationToken ct = default);
-    
     event EventHandler? OnDataReceived; 
+
+    #endregion
+    
 }
