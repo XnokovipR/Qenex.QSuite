6dac1ce967f4b5b27c440304f2ee53f640aaf3e4
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-03-31 20:19:38 +0200

    feat: add viewable treeview item

---
+++ QSuite/ViewModels/ViewableItem/IViewableItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Qenex.QSuite.ViewModels.ViewableItem
+{
+	public interface IViewableItem
+	{
+		string Label { get; set; }
+		BitmapImage Icon { get; }
+		ObservableCollection<IViewableItem> Children { get; set; }
+	}
+}
