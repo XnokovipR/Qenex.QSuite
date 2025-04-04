53d45c58e5a36592e8de05dda7f4d131d48d93aa
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-04-04 21:22:34 +0200

    feat: add project data structure

---
+++ QSuite/Models/Project/ProjectData.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Qenex.QLibs.XmlInOut;
+using Qenex.QSuite.Helpers.ProjectFile;
+using Qenex.QSuite.LogSystems.LogSystem;
+using Qenex.QSuite.ModuleXmlHandler.XmlStructure;
+
+namespace Qenex.QSuite.Models.Project;
+
+public class ProjectData
+{
+    private ILogger? logger { get; set; }
+    
+    #region Constructors
+    
+    public ProjectData(ILogger? logger = null)
+    {
+        this.logger = logger;
+    }
+    
+    #endregion
+    
+    
+    #region Project file properties
+
+    public XmlModule Module { get; set; } = null!;
+
+    #endregion
+
+
+    
+}
