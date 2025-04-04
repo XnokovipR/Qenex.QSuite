f9aff3d9e66a0b43f5fde0ee7e8a2cee11549643
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-04-04 21:29:29 +0200

    feat: implement ImageGetter for loading png file as BitmapImage

---
+++ QSuite/ViewModels/ViewableItem/ImageGetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace Qenex.QSuite.ViewModels.ViewableItem;
+
+internal class ImageGetter
+{
+    public static BitmapImage GetBitmapImage(string pathFile)
+    {
+        var uri = new Uri(@"pack://application:,,,/"
+                          + Assembly.GetCallingAssembly().GetName().Name
+                          + ";component/"
+                          + pathFile, UriKind.Absolute);
+
+        return new BitmapImage(uri);
+    }    
+}
