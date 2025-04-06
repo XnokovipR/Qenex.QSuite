1fdd93faf95cf29607a3e641713e05ef4f057754
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-04-06 22:35:08 +0200

    feat: use solutionExplorer wrapper to fill treeview

--- QSuite/ViewModels/SolutionExplorerViewModel.cs
+++ QSuite/ViewModels/SolutionExplorerViewModel.cs
@@ -1,52 +1,46 @@
-﻿using Qenex.QLibs.QUI;
+﻿using System.Collections.ObjectModel;
+using Qenex.QLibs.QUI;
+using Qenex.QSuite.Drivers.Driver;
 using Qenex.QSuite.LogSystems.LogSystem;
 using Qenex.QSuite.Models.Project;
+using Qenex.QSuite.Protocols.Protocol;
+using Qenex.QSuite.Variables.QVariables;
+using Qenex.QSuite.ViewModels.SolutionExplorerWrappers;
+using Qenex.QSuite.ViewModels.ViewableItem;
 using Syncfusion.Windows.Tools.Controls;
 
 namespace Qenex.QSuite.ViewModels;
 
-public class SolutionExplorerViewModel : ViewModelBase
+public class SolutionExplorerViewModel(EventAggregator ea) : ViewModelBase(ea)
 {
-    #region Private fileds
+    #region Private fields
 
-    private RealProjectData realProjectData;
+    private RealProjectData realProjectData = null!;
 
     #endregion
     
-    #region Constructors
-    
-    public SolutionExplorerViewModel(EventAggregator ea) : base(ea)
-    {
-    }
-    
+    #region TreeView properties
+
+    public ObservableCollection<IViewableItem> ProjectModules { get; set; } = [];
+
     #endregion
-    
-    #region Properties
 
-    #region BaseViewModel implementation
+    #region BaseViewModel Properties
 
-    public override string Header
-    {
-        get => "Solution Explorer";
-        set { }
-    }
+    public override string Identifier => $"SolutionExplorerViewModel:{Guid.NewGuid().ToString()}";
 
-    public override string Name
-    {
-        get => "SolutionExplorerViewModel";
-        set { }
-    }
+    public override string Header { get => "Solution Explorer"; set { } }
 
-    public override DockSide DockingPosition
-    {
-        get => DockSide.Left;
-        set { }
-    }
+    public override string Name { get => "SolutionExplorerViewModel"; set { } }
 
+    public override DockSide DockingPosition { get => DockSide.Left; set { } }
+
+    public override DockState DockState { get; set; }
+
+    public override bool CanMaximize => false;
     public override bool IsDocument => false;
     public override bool CanClose => false;
-
-    #endregion
+    public override bool CanSerialize => true;
 
     #endregion
 
@@ -54,17 +48,61 @@ public class SolutionExplorerViewModel : ViewModelBase
 
     public void ReloadProjectData(RealProjectData realPrjData)
     {
+        // Release all previous project data
+        DisposeAll();
+        
         realProjectData = realPrjData;
-        
-        // Actualize whole treeview
-        
+        var projectWrapper = CreateProjectWrapper(realPrjData);
+        ProjectModules.Add(projectWrapper);
     }
 
     public void DisposeAll()
     {
+        ProjectModules.Clear();
         realProjectData = null!;
+    }
 
-        // Dispose all treeview
+    #endregion
+
+    #region Create All Project Wrappers
+
+    private IViewableItem CreateProjectWrapper(RealProjectData realPrjData)
+    {
+        var projectWrapper = new ProjectWrapper(realPrjData.Module);
+        // Add drivers
+        CreateDriverWrappers(projectWrapper.Children, realPrjData.Module.Drivers);
+        return projectWrapper;
+    }
+    
+    private void CreateDriverWrappers(ObservableCollection<IViewableItem> children, IList<IDriverBase> drivers)
+    {
+        foreach (var driver in drivers)
+        {
+            var driverWrapper = new DriverWrapper(driver);
+            // Add protocols
+            CreateProtocolWrappers(driverWrapper.Children, driver.Protocols);
+            children.Add(driverWrapper);
+        }
+    }
+    
+    private void CreateProtocolWrappers(ObservableCollection<IViewableItem> children, IList<IProtocolBase> protocols)
+    {
+        foreach (var protocol in protocols)
+        {
+            var protocolWrapper = new ProtocolWrapper(protocol);
+            // Add variables
+            CreateVariableWrappers(protocolWrapper.Children, protocol.Variables);
+            children.Add(protocolWrapper);
+        }
+    }
+    
+    private void CreateVariableWrappers(ObservableCollection<IViewableItem> children, IList<IProtocolVariable> variables)
+    {
+        foreach (var variable in variables)
+        {
+            var variableWrapper = new ProtocolVariableWrapper(variable);
+            children.Add(variableWrapper);
+        }
     }
 
     #endregion
