37e931800b68c1028563629189f6cc6263716dbc
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-05-18 19:47:16 +0200

    ref: create new one..

---
+++ Controls/GraphControl/ViewModels/GraphControlViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media.Imaging;
+using Qenex.QSuite.Common.WpfComm;
+using Qenex.QSuite.Controls.Control;
+
+namespace Qenex.QSuite.Controls.GraphControl.ViewModels;
+
+public class GraphControlViewModel : ControlBase
+{
+    public override string ControlName => "GraphControl";
+    public override string Label => "Graph";
+    public override BitmapImage Icon => ImageGetter.GetBitmapImage("Icons/GraphControl.png");
+    public override string Description => "Graph Control for displaying data in a graphical format.";
+}
