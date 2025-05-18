17b11e39341eb607775a5c03cbc831c656d14cfc
Radek Pivokonský <radek.pivokonsky@gmail.com>
2025-05-18 19:57:44 +0200

    ref: old parts..

--- Controls/Control/ControlBase.cs
+++ Controls/Control/ControlBase.cs
@@ -1,5 +1,40 @@
-﻿namespace Qenex.QSuite.Controls.Control;
+﻿using System.Windows.Media.Imaging;
 
-public class ControlBase: IControlBase
+namespace Qenex.QSuite.Controls.Control;
+
+public abstract class ControlBase : IControlBase
 {
+	#region Private fiedlds
+
+	private const int DefaultHeight = 100;
+	private const int DefaultWidth = 100;
+
+	#endregion
+
+	#region Constructors
+	public ControlBase()
+	{
+		Tags = [];
+	}
+
+	#endregion
+
+	#region Properties
+	public abstract string ControlName { get; }
+	public abstract string Label { get; }
+	public abstract BitmapImage Icon { get; }
+	public abstract string Description { get; }
+	public virtual int MinHeight { get; } = DefaultHeight;
+	public virtual int MinWidth { get; } = DefaultWidth;
+
+	/// <summary>
+	/// Tags are used to store custom user control properties.
+	/// </summary>
+	public Dictionary<string, object> Tags { get; set; }
+	public int X { get; set; }
+	public int Y { get; set; }
+	public int Width { get; set; } = DefaultWidth;
+	public int Height { get; set; } = DefaultHeight;
+
+	#endregion
 }
--- Controls/Control/IControlBase.cs
+++ Controls/Control/IControlBase.cs
@@ -1,5 +1,45 @@
+﻿using System.Windows.Media.Imaging;
+
 namespace Qenex.QSuite.Controls.Control;
 
+/// <summary>
+/// Base interface for all controls which are used in workspaces.
+/// </summary>
 public interface IControlBase
 {
+	//todo: IControlBase - add acceptable variable types
+	//todo: IControlBase - add Variables (protocol or classical)
+
+	string ControlName { get; }
+	string Label { get; }
+	BitmapImage Icon { get; }
+	string Description { get; }
+	int MinHeight { get; }
+	int MinWidth { get; }
+
+
+	/// <summary>
+	/// Custom user control properties.
+	/// </summary>
+	Dictionary<string, object> Tags { get; set; }
+
+	/// <summary>
+	/// Control horizontal position of the left upper corner.
+	/// </summary>
+	int X { get; set; }
+
+	/// <summary>
+	/// Control vertical position of the left upper corner.
+	/// </summary>
+	int Y { get; set; }
+
+	/// <summary>
+	/// Control width.
+	/// </summary>
+	int Width { get; set; }
+
+	/// <summary>
+	/// Control height.
+	/// </summary>
+	int Height { get; set; }
 }
