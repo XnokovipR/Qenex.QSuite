302a0c5c45a080c5118fc525a6b86e4b234567f8
Radek Pivokonský <radek.pivokonsky@gmail.com>
2026-02-13 15:07:28 +0100

    ref: extract graph colors

---
+++ Controls/GraphControl/Helpers/ChartHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace Qenex.QSuite.Controls.GraphControl.Helpers;
+
+public class ChartHelper
+{
+    public static readonly Dictionary<int, System.Windows.Media.Color> ChartColors = new Dictionary<int, System.Windows.Media.Color>
+    {
+        { 1, System.Windows.Media.Colors.Yellow },
+        { 2, System.Windows.Media.Colors.Cyan },
+        { 3, System.Windows.Media.Colors.Magenta },
+        { 4, System.Windows.Media.Colors.Lime },
+        { 5, System.Windows.Media.Colors.Red },
+        { 6, System.Windows.Media.Colors.Silver },
+        { 7, System.Windows.Media.Colors.Coral },
+        { 8, System.Windows.Media.Colors.Blue },
+        { 9, System.Windows.Media.Colors.Green },
+        { 10, System.Windows.Media.Colors.Orange },
+        { 11, System.Windows.Media.Colors.Purple },
+        { 12, System.Windows.Media.Colors.Brown },
+        { 13, System.Windows.Media.Colors.Gray },
+        { 14, System.Windows.Media.Colors.Pink },
+        { 15, System.Windows.Media.Colors.Teal },
+        { 16, System.Windows.Media.Colors.Navy },
+        { 17, System.Windows.Media.Colors.Maroon },
+        { 18, System.Windows.Media.Colors.Olive },
+        { 19, System.Windows.Media.Colors.Gold },
+        { 20, System.Windows.Media.Colors.Turquoise }
+    };
+}
