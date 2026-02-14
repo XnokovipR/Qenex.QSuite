29ede3c37a6d10406d3ee1f06c21faaf7ca3e9d4
Radek Pivokonský <radek.pivokonsky@gmail.com>
2026-02-14 18:18:47 +0100

    feat: add mouse tracker

---
+++ Controls/Control/IHasMousePosition.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Qenex.QSuite.Controls.Control;
+
+public interface IHasMousePosition
+{
+    Point MousePosition { get; set; }
+}
---
+++ Controls/Control/MouseTrackerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Qenex.QSuite.Controls.Control;
+
+public static class MouseTrackerBehavior
+{
+    public static readonly DependencyProperty TrackProperty =
+        DependencyProperty.RegisterAttached(
+            "Track",
+            typeof(bool),
+            typeof(MouseTrackerBehavior),
+            new PropertyMetadata(false, OnTrackChanged));
+
+    public static void SetTrack(DependencyObject obj, bool value)
+        => obj.SetValue(TrackProperty, value);
+
+    public static bool GetTrack(DependencyObject obj)
+        => (bool)obj.GetValue(TrackProperty);
+
+    private static void OnTrackChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is ContentControl control)
+        {
+            if ((bool)e.NewValue)
+                CompositionTarget.Rendering += (_, __) =>
+                {
+                    if (control.DataContext is IHasMousePosition vm)
+                    {
+                        var pos = Mouse.GetPosition(control);
+                        vm.MousePosition = pos;
+                    }
+                };
+        }
+    }
+}
