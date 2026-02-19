d0ffa7c4dea6d9795a7256a9ab4c4af3108748ce
Radek Pivokonský <radek.pivokonsky@gmail.com>
2026-02-19 10:07:47 +0100

    feat: add input binding
    Textbox (and other) can react on enter confirm key

---
+++ Common/InpBindingManager/InputManager.cs
@@ -0,0 +1,79 @@
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Qenex.QSuite.Common.InpBindingManager;
+
+public static class InputManager
+    {
+        public static readonly DependencyProperty UpdatePropertySourceWhenEnterPressedProperty = DependencyProperty.RegisterAttached(
+            "UpdatePropertySourceWhenEnterPressed",
+            typeof(DependencyProperty),
+            typeof(InputManager),
+            new PropertyMetadata(null, OnUpdatePropertySourceWhenEnterPressedPropertyChanged)
+        );
+
+        static InputManager() { }
+
+        public static void SetUpdatePropertySourceWhenEnterPressed(DependencyObject dp, DependencyProperty value)
+        {
+            dp.SetValue(UpdatePropertySourceWhenEnterPressedProperty, value);
+        }
+
+        public static DependencyProperty GetUpdatePropertySourceWhenEnterPressed(DependencyObject dp)
+        {
+            return (DependencyProperty)dp.GetValue(UpdatePropertySourceWhenEnterPressedProperty);
+        }
+
+        private static void OnUpdatePropertySourceWhenEnterPressedPropertyChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+        {
+            UIElement element = dp as UIElement;
+
+            if (element == null)
+            {
+                return;
+            }
+
+            if (e.OldValue != null)
+            {
+                element.PreviewKeyDown -= HandlePreviewKeyDown;
+            }
+
+            if (e.NewValue != null)
+            {
+                element.PreviewKeyDown += new KeyEventHandler(HandlePreviewKeyDown);
+            }
+        }
+
+        static void HandlePreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                DoUpdateSource(e.Source);
+            }
+        }
+
+        static void DoUpdateSource(object source)
+        {
+            DependencyProperty property = GetUpdatePropertySourceWhenEnterPressed(source as DependencyObject);
+
+            if (property == null)
+            {
+                return;
+            }
+
+            UIElement elt = source as UIElement;
+
+            if (elt == null)
+            {
+                return;
+            }
+
+            BindingExpression binding = BindingOperations.GetBindingExpression(elt, property);
+
+            if (binding != null)
+            {
+                binding.UpdateSource();
+            }
+        }
+    }
