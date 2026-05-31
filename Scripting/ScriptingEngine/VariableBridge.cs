fa5b1ed6c877b08e0463a5e4b23d58d24136fe78
radek <radek.pivokonsly@gmail.com>
2026-05-31 13:19:51 +0200

    feat: add support for converting Python values to QVariable types

--- Scripting/ScriptingEngine/VariableBridge.cs
+++ Scripting/ScriptingEngine/VariableBridge.cs
@@ -1,4 +1,6 @@
-﻿using Qenex.QSuite.Variables.QVariables;
+using Qenex.QSuite.Variables.QVariables;
+using Python.Runtime;
+using System.Globalization;
 
 namespace Qenex.QSuite.Scripting.ScriptingEngine;
 
@@ -9,8 +11,34 @@ public class VariableBridge(IVariableBase variable)
     public object Value
     {
         get => variable.GetValue();
-        set => variable.SetValue(value);
+        set => variable.SetValue(ConvertValueForVariable(value));
     }
-    
+
     public override string ToString() => variable.GetValue().ToString() ?? "null";
+
+    private object ConvertValueForVariable(object value)
+    {
+        var currentValue = variable.GetValue();
+        if (currentValue == null)
+        {
+            return value;
+        }
+
+        var targetType = currentValue.GetType();
+        if (targetType.IsInstanceOfType(value))
+        {
+            return value;
+        }
+
+        if (value is PyObject pyObject)
+        {
+            using (Py.GIL())
+            {
+                return pyObject.AsManagedObject(targetType)
+                    ?? throw new InvalidCastException($"Cannot convert Python value of type {value.GetType()} to {targetType}.");
+            }
+        }
+
+        return Convert.ChangeType(value, targetType, CultureInfo.InvariantCulture);
+    }
 }
